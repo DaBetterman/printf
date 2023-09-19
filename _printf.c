1ca665b9282e4488b7efbd3a89a637a49f111d2a
DaBetterman <1ebrahimr@gmail.com>
2023-09-19 11:18:07 -0700

    changed code to accept i and d characters

--- _printf.c
+++ _printf.c
@@ -6,12 +6,16 @@
 */
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 #include "main.h"
+
 int _printf(const char *format, ...)
 {
-va_list args;
+int num;
 int printed_chars = 0;
+va_list args;
 va_start(args, format);
+
 while (*format)
 {
 if (*format != '%')
@@ -25,10 +29,26 @@ format++;
 switch (*format)
 {
 case 'c':
-print_char(&printed_chars, args);
+{
+char c = va_arg(args, int);
+putchar(c);
+printed_chars++;
+}
+break;
+case 'd':
+case 'i':
+{
+num = va_arg(args, int);
+printf("%d", num);
+printed_chars += 1;
+}
 break;
 case 's':
-print_string(&printed_chars, args);
+{
+const char *str = va_arg(args, const char *);
+fputs(str, stdout);
+printed_chars += strlen(str);
+}
 break;
 case '%':
 putchar('%');
